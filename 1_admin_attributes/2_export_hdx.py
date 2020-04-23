a6059b1342fc7bba6d52cf16d72884190c2048b4
Max Malynowsky <maxmalynowsky@gmail.com>
2020-04-23 09:33:58 -0400

    Export to HDX schema
    Former-commit-id: d2026e31c62dde932105cc2840db581a44750dd4 [formerly 983be42be617846d15121f22e6e54ffed6bf4d7c]
    Former-commit-id: 247c428b6d4dfca6157032fce473cf7b52ed3170

--- 1_admin_attributes/2_export_hdx.py
+++ 1_admin_attributes/2_export_hdx.py
@@ -56,6 +56,24 @@ def format_adm(attr, row, level, langs):
     return df
 
 
+def drop_col(join, level, max_level, langs):
+    cols = ['id_0', 'id_1', 'id_2', 'id_3', 'id_4', 'id_5']
+    for lvl in range(max_level, -1, -1):
+        if lvl == level:
+            continue
+        if lvl > level:
+            for lang in langs:
+                cols.append(f'admin{lvl}Name_{lang}')
+            cols.append(f'admin{lvl}Pcode')
+        cols.append(f'admin{lvl}RefName')
+        for lang in langs:
+            cols.append(f'admin{lvl}AltName1_{lang}')
+            cols.append(f'admin{lvl}AltName2_{lang}')
+    df = join.drop(columns=cols)
+    df = df.dropna(subset=[f'admin{level}Pcode'])
+    return df
+
+
 na_values = ['', '#N/A']
 
 cwd = Path(__file__).parent
@@ -85,13 +103,12 @@ for index, row in db['adm0'].iterrows():
         lvl = format_adm(attr, row, level, langs)
         join = join.merge(lvl, how='outer', on=f'id_{level}')
     join = join.merge(get_adm0(row, langs), how='outer', on='id_0')
+    drop_levels = levels + [0]
+    for level in (drop_levels):
+        cty[f'Admin{level}'] = drop_col(join, level, drop_levels[0], langs)
+    ids = map(lambda x: f'id_{x}', range(5, drop_levels[0], -1))
+    join = join.drop(columns=ids)
     cty['join'] = join
-
-    for level in levels:
-        adm = f'adm{level}'
-        attr = db[adm][db[adm].id.str.contains(row['id'], na=False)]
-        lvl = format_adm(attr, row, level, langs)
-        join = join.merge(lvl, how='outer', on=f'id_{level}')
 
     output = f"2_export_hdx/{row['id'].lower()}.xlsx"
     writer = pd.ExcelWriter((cwd / output).resolve(), engine='xlsxwriter')
