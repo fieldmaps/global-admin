c43d4887f9d7bacbf9c8a0a984b690475947146a
Max Malynowsky <github@maxmalynowsky.com>
2021-01-08 19:26:01 +0100

    export geojsonl to tiles data
    Former-commit-id: bb567b97c4d0e67c52a3ad477d753e81ff4b4f8b [formerly 04d5fce128d7c22083cc84a1f5872476e9601aad]
    Former-commit-id: 1685cda0a74e4921a7de6af934d5cc05e542f09a

--- 3_exports/11_geojsonl.py
+++ 3_exports/11_geojsonl.py
@@ -2,14 +2,24 @@ import os
 from pathlib import Path
 
 cwd = Path(__file__).parent
-output_path = (cwd / '11_geojsonl').resolve()
+output_path = (cwd / '../../tiles/data').resolve()
 output_path.mkdir(parents=True, exist_ok=True)
 
-for layer in ['points', 'lines', 'polygons']:
-    output = (output_path / f'wld_{layer}.geojsonl').resolve()
-    print(f'wld_{layer}.gpkg.zip')
+for layer in ['points', 'lines']:
     input = (cwd / f'09_export_merge/wld_{layer}.gpkg').resolve()
-    os.system(
-        f"""ogr2ogr \
-        {output} {input}"""
-    )
+    if layer == 'lines':
+        output = (output_path / f'land.geojsonl').resolve()
+        print(output)
+        os.system(
+            f"""ogr2ogr \
+            -sql "SELECT * FROM land" \
+            {output} {input}"""
+        )
+    for level in range(5):
+        output = (output_path / f'adm{level}_{layer}.geojsonl').resolve()
+        print(output)
+        os.system(
+            f"""ogr2ogr \
+            -sql "SELECT * FROM 'adm{level}'" \
+            {output} {input}"""
+        )
